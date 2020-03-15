a61f1a599f5071c78a5c57b8484278238f80574a
thsc42 <thomas.schwotzer@htw-berlin.de>
2020-03-15 10:12:33 +0100

    moved era calculation to a central (algebra) class.

--- src/net/sharksystem/asap/ASAP.java
+++ src/net/sharksystem/asap/ASAP.java
@@ -4,14 +4,14 @@ public class ASAP {
     public static final int INITIAL_ERA = 0;
     public static final int MAX_ERA = Integer.MAX_VALUE;
 
-    static int nextEra(int workingEra) {
+    public static int nextEra(int workingEra) {
         if(workingEra == ASAP.MAX_ERA) {
             return ASAP.INITIAL_ERA;
         }
         return workingEra+1;
     }
 
-    static int previousEra(int workingEra) {
+    public static int previousEra(int workingEra) {
         if(workingEra == ASAP.INITIAL_ERA) {
             return ASAP.MAX_ERA;
         }
