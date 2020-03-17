f56c1c634031f3cb9b54040251f859e7e6532f5a
thsc42 <thomas.schwotzer@htw-berlin.de>
2020-03-17 14:15:30 +0100

    stabilizing certificate exchange in sn2

--- src/net/sharksystem/asap/ASAPEngine.java
+++ src/net/sharksystem/asap/ASAPEngine.java
@@ -166,16 +166,16 @@ public abstract class ASAPEngine implements ASAPStorage, ASAPProtocolEngine, ASA
 
     @Override
     public void add(CharSequence urlTarget, byte[] messageAsBytes) throws IOException {
-        Log.writeLog(this, "reached add(uri, byte[] message");
+//        Log.writeLog(this, "reached add(uri, byte[] message");
         ASAPChunk chunk = this.chunkStorage.getChunk(urlTarget, this.era);
 
-        Log.writeLog(this, "call chunk.addMessage()");
+//        Log.writeLog(this, "call chunk.addMessage()");
         chunk.addMessage(messageAsBytes);
 
         // remember - something changed in that era
         this.contentChanged();
 
-        Log.writeLog(this, "online?");
+//        Log.writeLog(this, "online?");
         if(this.asapOnlineMessageSender != null) {
             try {
                 Log.writeLog(this, "send online message...");
