dbffd6c11dd08143b57d6c7415adfc4c5152e1cd
thsc42 <thomas.schwotzer@htw-berlin.de>
2020-09-22 15:39:45 +0200

    that crypto architecture is still a mess.

---
+++ src/net/sharksystem/crypto/BasicCryptoSettings.java
@@ -0,0 +1,53 @@
+package net.sharksystem.crypto;
+
+import net.sharksystem.asap.ASAPSecurityException;
+
+import javax.crypto.SecretKey;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+
+public interface BasicCryptoSettings {
+    String DEFAULT_RSA_ENCRYPTION_ALGORITHM = "RSA/ECB/PKCS1Padding";
+    String DEFAULT_SYMMETRIC_KEY_TYPE = "AES";
+    String DEFAULT_SYMMETRIC_ENCRYPTION_ALGORITHM = "AES/ECB/PKCS5Padding";
+    //    public static int DEFAULT_AES_KEY_SIZE = 256;
+    int DEFAULT_AES_KEY_SIZE = 128; // TODO we can do better
+    String DEFAULT_SIGNATURE_ALGORITHM = "SHA256withRSA";
+
+    /**
+     *
+     * @return private key of local device - for signing
+     * @throws ASAPSecurityException
+     */
+    PrivateKey getPrivateKey() throws ASAPSecurityException;
+
+    // debugging
+//    PrivateKey getPrivateKey(CharSequence subjectID) throws ASAPSecurityException;
+
+    /**
+     * @return public key of local device - for signing
+     * @throws ASAPSecurityException
+     */
+    PublicKey getPublicKey() throws ASAPSecurityException;
+
+    String getRSAEncryptionAlgorithm();
+
+    String getRSASigningAlgorithm();
+
+    SecretKey generateSymmetricKey() throws ASAPSecurityException;
+
+    String getSymmetricEncryptionAlgorithm();
+
+    String getSymmetricKeyType();
+
+    int getSymmetricKeyLen();
+
+    /**
+     *
+     * @param peerID
+     * @return true if peerID is owners' id.
+     */
+    boolean isOwner(CharSequence peerID);
+
+    CharSequence getOwner();
+}
--- src/net/sharksystem/crypto/BasicKeyStore.java
+++ src/net/sharksystem/crypto/BasicKeyStore.java
@@ -6,56 +6,11 @@ import javax.crypto.SecretKey;
 import java.security.PrivateKey;
 import java.security.PublicKey;
 
-public interface BasicKeyStore {
-    String DEFAULT_RSA_ENCRYPTION_ALGORITHM = "RSA/ECB/PKCS1Padding";
-    String DEFAULT_SYMMETRIC_KEY_TYPE = "AES";
-    String DEFAULT_SYMMETRIC_ENCRYPTION_ALGORITHM = "AES/ECB/PKCS5Padding";
-    //    public static int DEFAULT_AES_KEY_SIZE = 256;
-    int DEFAULT_AES_KEY_SIZE = 128; // TODO we can do better
-    String DEFAULT_SIGNATURE_ALGORITHM = "SHA256withRSA";
-
+public interface BasicKeyStore extends BasicCryptoSettings {
     /**
-     *
-     * @return private key of local device - for signing
-     * @throws ASAPSecurityException
-     */
-    PrivateKey getPrivateKey() throws ASAPSecurityException;
-
-    // debugging
-//    PrivateKey getPrivateKey(CharSequence subjectID) throws ASAPSecurityException;
-
-    /**
-     *
      * @param subjectID
      * @return public key of recipient - to encrypt
      * @throws ASAPSecurityException if key cannot be found
      */
     PublicKey getPublicKey(CharSequence subjectID) throws ASAPSecurityException;
-
-    /**
-     * @return public key of local device - for signing
-     * @throws ASAPSecurityException
-     */
-    PublicKey getPublicKey() throws ASAPSecurityException;
-
-    String getRSAEncryptionAlgorithm();
-
-    String getRSASigningAlgorithm();
-
-    SecretKey generateSymmetricKey() throws ASAPSecurityException;
-
-    String getSymmetricEncryptionAlgorithm();
-
-    String getSymmetricKeyType();
-
-    int getSymmetricKeyLen();
-
-    /**
-     *
-     * @param peerID
-     * @return true if peerID is owners' id.
-     */
-    boolean isOwner(CharSequence peerID);
-
-    CharSequence getOwner();
 }
