fd1c8a9ba5886d12c1f76ab0c37713cc3f44e213
thsc42 <thomas.schwotzer@htw-berlin.de>
2021-02-01 10:16:46 +0100

    created bugreports sub folder

---
+++ test/bugreports/BugReports.java
@@ -0,0 +1,21 @@
+package bugreports;
+
+import org.junit.Test;
+
+/**
+ * Any bug reports is very welcome and appreciated.
+ */
+public class BugReports {
+    @Test
+    public void perfectSoftware() {
+        System.out.println("There is no such thing as perfect software. It is only less buggy. Thanks for any help.");
+    }
+
+    /**
+     * Add your report here - thank you
+     */
+    @Test
+    public void yourBugReport() {
+        System.out.println("Thanks for helping!!");
+    }
+}
