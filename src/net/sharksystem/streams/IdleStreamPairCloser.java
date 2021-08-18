b7b396a9ea9050835789169394e763f8d1208a10
thsc42 <thomas.schwotzer@htw-berlin.de>
2021-08-18 11:25:03 +0200

    added idle channel closer

---
+++ src/net/sharksystem/streams/IdleStreamPairCloser.java
@@ -0,0 +1,60 @@
+package net.sharksystem.streams;
+
+import net.sharksystem.utils.AlarmClock;
+import net.sharksystem.utils.AlarmClockListener;
+
+import java.io.IOException;
+
+public class IdleStreamPairCloser implements AlarmClockListener, WrappedStreamPairListener {
+    private final int timeout;
+    private StreamPairWrapper streamPairWrapper;
+    private AlarmClock alarmClock;
+
+    private IdleStreamPairCloser(int timeout) {
+        this.timeout = timeout;
+    }
+
+    public static IdleStreamPairCloser getIdleStreamsCloser(StreamPair streamPair, int timeout) throws IOException {
+        // create object that also becomes listener
+        IdleStreamPairCloser idleStreamPairCloser = new IdleStreamPairCloser(timeout);
+
+        // create wrapper with listener
+        StreamPairWrapper streamPairWrapper = new StreamPairWrapper(
+                streamPair.getInputStream(), streamPair.getOutputStream(), idleStreamPairCloser, "X");
+
+        // now: put wrapper into listener
+        idleStreamPairCloser.setStreamPairWrapper(streamPairWrapper);
+        return idleStreamPairCloser;
+    }
+
+    void setStreamPairWrapper(StreamPairWrapper streamPairWrapper) {
+        this.streamPairWrapper = streamPairWrapper;
+    }
+
+    public void start() {
+        this.alarmClock = new AlarmClock(this.timeout, this);
+    }
+
+    @Override
+    public void alarmClockRinging(int i) {
+        try {
+            this.streamPairWrapper.getInputStream().close();
+        } catch (IOException e) {
+            // ignore
+        }
+    }
+
+    @Override
+    public void notifyClosed(StreamPair streamPair, String s) {
+        this.alarmClock.kill(); // nothing todo.
+        this.alarmClock = null;
+    }
+
+    @Override
+    public void notifyAction(String s) {
+        if(alarmClock != null) {
+            this.alarmClock.kill();
+            this.alarmClock = new AlarmClock(timeout, this);
+        } // else not yet started
+    }
+}
